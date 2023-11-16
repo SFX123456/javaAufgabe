cf95e46f6aad081e3ce274f8bb738e047d092294
SFX123456 <127447985+SFX123456@users.noreply.github.com>
2023-11-16 23:27:58 +0100

    Update Reader.java

--- src/Reader.java
+++ src/Reader.java
@@ -11,30 +11,32 @@ public class Reader {
 	{
 		File file = new File("./bin/ArtikeldateiNozama.txt");
 		DefaultListModel<Artikel> list = new DefaultListModel<Artikel>();
-        try
-        {
-        	Scanner sc = new Scanner(new FileReader(file));
-            sc.nextLine();
-            String str;
-            while(sc.hasNextLine())
-            {
-            	str = sc.nextLine();
-                String[] arr = str.split(";");
-                int artikelNummer = Integer.parseInt(arr[0]);
-                String artikelBezeichnung = arr[1].trim();
-                int index = arr[2].indexOf(",");
-                char waehrung = arr[2].charAt(index + 4);
-                String preisMitKomma = arr[2].replace(',','.');
-                double preis =Double.valueOf(preisMitKomma.substring(1,index +2)).doubleValue();
-                Artikel artikel = new Artikel(artikelNummer, artikelBezeichnung, preis, waehrung);
-                list.addElement(artikel);
-            }
-            sc.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Konnte Datei nicht finden");
-        }
+        	try
+	        {
+        		Scanner sc = new Scanner(new FileReader(file));
+            		sc.nextLine();
+            		String str;
+            		while(sc.hasNextLine())
+	            	{
+            			str = sc.nextLine();
+                		String[] arr = str.split(";");
+                		int artikelNummer = Integer.parseInt(arr[0]);
+                		String artikelBezeichnung = arr[1].trim();
+                		int index = arr[2].indexOf(",");
+                		char waehrung = arr[2].charAt(index + 4);
+                		String preisMitKomma = arr[2].replace(',','.');
+                		double preis =Double.valueOf(preisMitKomma.substring(1,index +2)).doubleValue();
+                		Artikel artikel = new Artikel(artikelNummer, artikelBezeichnung, preis, waehrung);
+                		list.addElement(artikel);
+            		}
+            		sc.close();
+        	} 
+		catch (FileNotFoundException e) 
+		{
+            	System.out.println("Konnte Datei nicht finden");
+        	}
         
        
-        return list;
+        	return list;
 	}
 }
